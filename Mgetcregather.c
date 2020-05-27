b15de7983b3e64b5d8fb61d875259ae21028cb28
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-05-26 22:51:15 -0300

    Use an aperture parameter in sfgetcregather #38
    The 'aperture' parameter in 'Mgetcregather.c' defines the maximun
    number of traces in a cre gather. This is used to avoid generation
    of a big cre gather with many traces that won't be used in stacking.
    
    It also avoid to take offset coordinates that have CMPs far away from
    Central CMP m0 that may cause a bug. For a given offset h, the cmp
    coordinate m could be outside of the datacube because the CRE trajectory
    in m,h plane is a parabola and CMP coordinates increases fast in relation
    to offset.

--- Mgetcregather.c
+++ Mgetcregather.c
@@ -32,10 +32,11 @@ int main(int argc, char* argv[])
 	float ot; // Time axis origin
 	int nt; // Number of time samples
 	int i,j; // loop counter
-	int cre_n; // Number of CRE Gather traces
+	int cre_n; // Number of traces in CRE vector
 	float cre_d; // CRE Gather sampling
 	float cre_o; // CRE Gather axis origin
 	int trac_m; // CMP sample index
+	int aperture; // Number of traces in CRE Gather
 
 	/* RSF files I/O */  
 	sf_file in, out, out_m, cremh;
@@ -64,6 +65,13 @@ int main(int argc, char* argv[])
 	if(!sf_histfloat(cremh,"d1",&cre_d)) sf_error("No d1= in cremh file");
 	if(!sf_histfloat(cremh,"o1",&cre_o)) sf_error("No o1= in cremh file");
 
+	if(!sf_getint("aperture",&aperture)) aperture=1;
+	/* Number of traces in a CRE Gather*/
+
+	if(aperture > cre_n){
+		sf_error("The aperture can't be > n1 in cremh file\naperture=%i n2=%i",aperture,cre_n);
+	}
+
 	if(! sf_getbool("verb",&verb)) verb=0;
 	/* 1: active mode; 0: quiet mode */
 
@@ -84,9 +92,9 @@ int main(int argc, char* argv[])
 	
 	m = sf_floatalloc(cre_n);
 	sf_floatread(m,cre_n,cremh);
-	creGather = sf_floatalloc2(nt,cre_n);
+	creGather = sf_floatalloc2(nt,aperture);
 
-	for(i=0;i<cre_n;i++){
+	for(i=0;i<aperture;i++){
 		trac_m = (int)((double)m[i]/dm);
 
 		for(j=0;j<nt;j++){
@@ -96,14 +104,14 @@ int main(int argc, char* argv[])
 
 	/* eixo = sf_maxa(n,o,d)*/
 	ax = sf_maxa(nt, ot, dt);
-	ay = sf_maxa(nh, oh, dh);
+	ay = sf_maxa(aperture, oh, dh);
 	az = sf_maxa(1, 0, 1);
 
 	/* sf_oaxa(arquivo, eixo, índice do eixo) */
 	sf_oaxa(out,ax,1);
 	sf_oaxa(out,ay,2);
 	sf_oaxa(out,az,3);
-	sf_floatwrite(creGather[0],cre_n*nt,out);
+	sf_floatwrite(creGather[0],aperture*nt,out);
 
 	/* eixo do vetor m */
 	am = sf_maxa(cre_n,oh,dh);
