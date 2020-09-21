0643e03cdc12789346769b37405d0aabda903409
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-09-21 19:21:01 -0300

    Scratch of the program sfrnip2vrms #54
    Scratch of the program, input, output and selfdoc

--- Mrnip2vrms.c
+++ Mrnip2vrms.c
@@ -1,6 +1,10 @@
 /* Version 1.0 - Estimate root mean square velocity (VRMS) from RNIP
 
-more text
+ This program is based on multifocus formula that allow to estimate
+ VRMS from RNIP: vrms = sqrt(2*RNIP*v0/t0)
+
+ Reference: Diffraction imaging by multifocusing, Berkovitch A.,
+ Belfer I., Hassin Y., and Landa E., 2009.
 
  Programer: Rodolfo A C Neves (Dirack) 21/09/2020
  
@@ -9,19 +13,25 @@ more text
  License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 */
 
+#include <math.h>
 #include <rsf.h>
 /*^*/
 
 int main(int argc,char* argv[]){
 	
-	bool verb;
-	int nt0;
-	float dt0;
-	float ot0;
-	int nm0;
-	float dm0;
-	float om0;
-	int nrnip;
+	bool verb; // Verbose parameter
+	float t0; // t0 sample in stacked section
+	int nt0; // Number of t0's in stacked section
+	float dt0; // Time sampling in stacked section
+	float ot0; // Origin of the time axis in stacked section
+	int nm0; // Number of CMP's in stacked section
+	float dm0; // CMP sampling in stacked section
+	float om0; // Origin of the CMP axis in stacked section
+	int nrnip; // Number of samples in rnip vector
+	float v0; // Near surface velocity
+	float* rnip; // rnip vector
+	float** vrmsSection; // VRMS Section (t0,m0,vrms)
+	int i, j; // loop counter
 
 	sf_file in,rnip_file,out;
 	sf_init(argc,argv);
@@ -43,6 +53,9 @@ int main(int argc,char* argv[]){
 
 	if(!sf_getbool("verb",&verb)) verb=0;
 	/*1: verbose; 0: quiet*/
+
+	if(!sf_getfloat("v0",&v0)) v0=1.5;
+	/* Near surface velocity (Km/s) */
 	
 	if(verb){
 		sf_warning("Input file (Stacked Section)");
@@ -50,10 +63,29 @@ int main(int argc,char* argv[]){
 		sf_warning("nm0=%f dm0=%f ot0=%f",nm0,dm0,om0);
 		sf_warning("RNIP's file");
 		sf_warning("n1=%f",nrnip);
+		sf_warning("Command line parameters");
+		sf_warning("v0=%f",v0);
 	}
 
+	/* Assure that each (t0,m0) pair has a rnip value */
+	if(!((nm0*nt0)==nrnip))
+	sf_error("Dimension n1 of rnip vector should be equal n1*n2 in stacked section");
+
+	/* Load rnip values */
+	rnip = sf_floatalloc(nm0*nt0);
+	sf_floatread(rnip,nm0*nt0,rnip_file);
+	vrmsSection = sf_floatalloc2(nt0,nm0);
+
 	/* Calculate VRMS from RNIP */
+	for(j=0;j<nm0;j++){
+		
+		for(i=0;i<nt0;i++){
+			
+			t0 = nt0*i+ot0;
+			vrmsSection[j][i]=sqrt((2*rnip[(j*nt0)+i]*v0)/t0);
+		}
+	}
 
 	/* Output the VRMS section */
-
+	sf_floatwrite(vrmsSection[0],nt0*nm0,out);
 }
