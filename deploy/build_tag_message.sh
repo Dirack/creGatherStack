ed329a0ffcec06a6b6ef6fa7c3789b2fe4e5d693
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-10-11 16:33:37 -0300

    [ADD] Script to generate tag message
    The script deploy/build_tag_message.sh generate an automatic tag
    message using git log and commit marks in the body or subject of
    relevant commits in the history.
    
    The list of marks and its meaning:
    
     - [ADD] Added for new features.
     - [CHA] Changed for changes in existing functionality.
     - [DEP] Deprecated for once-stable features removed in upcoming releases.
     - [REM] Removed for deprecated features removed in this release.
     - [FIX] Fixed for any bug fixes.
     - [SEC] Security to invite users to upgrade in case of vulnerabilities.
    
    They should be added to the beggining of the commit subject to be
    added to the formated tag message and changelog during deploy

---
+++ deploy/build_tag_message.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# - ADD Added for new features.
+# - CHA Changed for changes in existing functionality.
+# - DEP Deprecated for once-stable features removed in upcoming releases.
+# - REM Removed for deprecated features removed in this release.
+# - FIX Fixed for any bug fixes.
+# - SEC Security to invite users to upgrade in case of vulnerabilities.
+
+PREVIOUS_TAG=$(git tag | sort -n -r | head -1)
+CURRENT_TAG=$(cat VERSION.md)
+
+# TAG HEADER
+echo "VERSION $CURRENT_TAG"
+
+# TAG MESSAGE 
+git log HEAD..."$PREVIOUS_TAG" --grep='\[ADD\]' --pretty='format:(%h) %s%n%b'
+git log HEAD..."$PREVIOUS_TAG" --grep='\[CHA\]' --pretty='format:(%h) %s%n%b'
+git log HEAD..."$PREVIOUS_TAG" --grep='\[DEP\]' --pretty='format:(%h) %s%n%b'
+git log HEAD..."$PREVIOUS_TAG" --grep='\[REM\]' --pretty='format:(%h) %s%n%b'
+git log HEAD..."$PREVIOUS_TAG" --grep='\[FIX\]' --pretty='format:(%h) %s%n%b'
+git log HEAD..."$PREVIOUS_TAG" --grep='\[SEC\]' --pretty='format:(%h) %s%n%b'
+
