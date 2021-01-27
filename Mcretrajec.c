052f2bbff2334d1b3d138c16d8b4e9ac21126330
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-01-27 16:35:26 -0300

    Adapt the sfcretraject to the new vfsa version #61
    Now the program sfcretraject receives a parameters cube from
    sfvfsacrsnh program and it calculates all cre trajectories in
    one iteration for each (t0,m0) pair in the parameters cube

--- Mcretrajec.c
+++ Mcretrajec.c
@@ -16,14 +16,19 @@ License: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 int main(int argc, char* argv[])
 {
 
-	float* m; // CMP
+	float** m; // CMP
 	float h; // Half offset
+	float m0;
 	double alpha; // Assymetry parameter
-	float m0; // central CMP
-	float* p; // RNIP and BETA parameters temporary vector
-	float RNIP; 
-	float BETA;
-	int np; // Number of parameters
+	float om0;
+	float dm0;
+	int nm0;
+	float ot0;
+	float dt0;
+	int nt0;
+	float** p; // RNIP and BETA parameters temporary vector
+	int np1; // Number of parameters in parameters file
+	int np2; // Number of (t0, m0) pairs in parameters file
 	bool verb;
 	float dm; // CMP sampling
 	float om; // CMP axis origin
@@ -34,7 +39,7 @@ int main(int argc, char* argv[])
 	float dt; // Time sampling
 	float ot; // Time axis origin
 	int nt; // Number of time samples
-	int i; // counter
+	int i,l,k; // counter
 
 	/* RSF files I/O */  
 	sf_file in, out, par;
@@ -48,21 +53,30 @@ int main(int argc, char* argv[])
 	par = sf_input("param"); // RNIP and BETA parameters
 	out = sf_output("out"); // m(h) vector
 
-	if (!sf_getfloat("m0",&m0)) m0=0;
-	/* central CMP (Km) */
+	if (!sf_getint("nm0",&nm0)) sf_error("Need nm0");
+	/* Number of central CMPs in parameters file */
+	if (!sf_getfloat("om0",&om0)) sf_error("Need om0");
+	/* First central CMP coordinate in parameters file (Km) */
+	if (!sf_getfloat("dm0",&dm0)) sf_error("Need dm0");
+	/* Central CMPs sampling in parameters file (Km) */
+	if (!sf_getint("nt0",&nt0)) sf_error("Need nt0");
+	/* Number of t0s in parameters file */
+	if (!sf_getfloat("ot0",&ot0)) sf_error("Need ot0");
+	/* First t0 coordinate in parameters file (s) */
+	if (!sf_getfloat("dt0",&dt0)) sf_error("Need dt0"); 
+	/* t0s sampling in parameters file (s) */
 
-	if (!sf_histint(par,"n1",&np)){
-		sf_error("No n1= in parameters file");
-	}else if(np < 3){
-		sf_error("The number of paramters should be 3 at least: RN, RNIP and BETA");
-	}else{
-		p = sf_floatalloc(np);
-		sf_floatread(p,np,par);
-		RNIP = p[1];
-		if(RNIP == 0) sf_error("RNIP can't be zero");
-		BETA = p[2];
-	}
+	/* Parameters file */
+	if (!sf_histint(par,"n1",&np1)) sf_error("No n1= in parameters file");
+	if (!sf_histint(par,"n2",&np2)) sf_error("No n2= in parameters file");
 
+	/* Check dimensions and input */
+	if ((nm0*nt0) != np2) 
+		sf_error("nm0*nt0 should be equal to n2 in parameters file!");
+	if(np1 < 3) 
+		sf_error("It should have at least 3 parameters in parameters file!");
+
+	/* seismic data cube A(m,h,t) */
 	if (!sf_histint(in,"n1",&nt)) sf_error("No n1= in input file");
 	if (!sf_histfloat(in,"d1",&dt)) sf_error("No d1= in input file");
 	if (!sf_histfloat(in,"o1",&ot)) sf_error("No o1= in input file");
@@ -76,46 +90,55 @@ int main(int argc, char* argv[])
 	if(! sf_getbool("verb",&verb)) verb=0;
 	/* 1: active mode; 0: quiet mode */
 
-	alpha = sin(BETA)/RNIP;
-
 	if (verb) {
 
 		sf_warning("Active mode on!!!");
 		sf_warning("Command line parameters: "); 
-		sf_warning("m0=%f",m0);
-		sf_warning("Input file parameters: ");
-		sf_warning("RNIP=%f BETA=%f",RNIP,BETA);
+		sf_warning("nm0=%d om0=%f dm0=%f",nm0,om0,dm0);
+		sf_warning("nt0=%d ot0=%f dt0=%f",nt0,ot0,dt0);
+		sf_warning("Parameters file: ");
+		sf_warning("n1=%d n2=%d",np1,np2);
 		sf_warning("Data cube dimensions: ");
-		sf_warning("n1=%i d1=%f o1=%f",nt,dt,ot);
-		sf_warning("n2=%i d2=%f o2=%f",nh,dh,oh);
-		sf_warning("n3=%i d3=%f o3=%f",nm,dm,om);
-		sf_warning("Calculated assimetry parameter:");
-		sf_warning("alpha=%f", alpha);
+		sf_warning("n1=%d d1=%f o1=%f",nt,dt,ot);
+		sf_warning("n2=%d d2=%f o2=%f",nh,dh,oh);
+		sf_warning("n3=%d d3=%f o3=%f",nm,dm,om);
 	}
 	
-	m = sf_floatalloc(nh);
+	m = sf_floatalloc2(nh,np2);
+	p = sf_floatalloc2(np1,np2);
+	sf_floatread(p[0],np1*np2,par);
 
-	if(alpha <= 0.001 && alpha >= -0.001){
-		for(i=0;i<nh;i++){
-			m[i] = m0;
-		}
-	}else{
-		for(i=0;i<nh;i++){
-			h = (dh*i) + oh;
-			m[i] = m0 + (1/(2*alpha)) * (1 - sqrt(1 + 4 * alpha * alpha * h * h));
-		}
-	}
+	for(l=0;l<nm0;l++){
+
+		m0 = l*dm0+om0;
+
+		for(k=0;k<nt0;k++){
+
+			alpha = sin(p[(l*nt0)+k][2])/p[(l*nt0)+k][1];
+
+			if(alpha <= 0.001 && alpha >= -0.001){
+				for(i=0;i<nh;i++){
+					m[(l*nt0)+k][i] = m0;
+				}
+			}else{
+				for(i=0;i<nh;i++){
+					h = (dh*i) + oh;
+					m[(l*nt0)+k][i] = m0 + (1/(2*alpha)) * (1 - sqrt(1 + 4 * alpha * alpha * h * h));
+				}
+			}
+		}/* loop over t0s */
+	}/*loop over m0s */
 
 	/* axis = sf_maxa(n,o,d)*/
 	ax = sf_maxa(nh, oh, dh);
-	ay = sf_maxa(1, 0, 1);
+	ay = sf_maxa(np2, 0, 1);
 	az = sf_maxa(1, 0, 1);
 
 	/* sf_oaxa(file, axis, axis index) */
 	sf_oaxa(out,ax,1);
 	sf_oaxa(out,ay,2);
 	sf_oaxa(out,az,3);
-	sf_floatwrite(m,nh,out);
+	sf_floatwrite(m[0],nh*np2,out);
 
 	exit(0);
 }
